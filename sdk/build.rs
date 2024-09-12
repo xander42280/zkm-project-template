bc118e0d229ef43284df1c6aff4d08b9f21a7cfa
xander42280 <xander.z@zkm.io>
2024-09-12 16:58:32 +0800

    remove lib

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rustc-link-search=native=./local/libsnark");
-    println!("cargo:rustc-link-lib=dylib=libsnark");
+    // println!("cargo:rustc-link-search=native=./local/libsnark");
+    // println!("cargo:rustc-link-lib=dylib=libsnark");
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["src/proto/stage.proto"], &["src/proto"])?;
